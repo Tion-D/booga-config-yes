c6fa1923407b89c6ecc027c564600a28f214e7d7
Tion_D <84892657+Tion-D@users.noreply.github.com>
2025-09-21 03:55:06 -0400

    Update booga.lua

--- booga.lua
+++ booga.lua
@@ -1,4 +1,5 @@
---setthreadidentity(5)
+-- setthreadidentity(5)
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local events = ReplicatedStorage:FindFirstChild("Events")
 if events then
