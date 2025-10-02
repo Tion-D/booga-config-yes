072749141f743ffdf35f59e9ca2d5f8d1a628af6
Tion_D <84892657+Tion-D@users.noreply.github.com>
2025-10-01 21:16:17 -0400

    Update booga.lua

--- booga.lua
+++ booga.lua
@@ -2785,3 +2785,20 @@ Workspace.Deployables.ChildRemoved:Connect(function(deployable)
         cavenodeRun = false
     end
 end)
+
+local GC = getconnections or get_signal_cons
+if GC then
+    for i,v in pairs(GC(Players.LocalPlayer.Idled)) do
+        if v["Disable"] then
+            v["Disable"](v)
+        elseif v["Disconnect"] then
+            v["Disconnect"](v)
+        end
+    end
+else
+    local VirtualUser = cloneref(game:GetService("VirtualUser"))
+    Players.LocalPlayer.Idled:Connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+end
